3186b916d6e144b3cb0dd8230990ad5aae45925b
David Bucciarelli <dade916@gmail.com>
2017-11-29 17:49:49 +0100

    Initial release

---
+++ download.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+DIST=distfiles
+
+function verify_hash() {
+	local hash=$1
+	local filepath=$2
+	echo "$hash $filepath" | sha1sum --status -c -
+	return $?
+}
+
+echo " * Checking distfiles"
+for LINE in `cat files` ; do
+	FILE=`echo $LINE | cut -d'@' -f1`
+	HASH=`echo $LINE | cut -d'@' -f2`
+	URL=`echo $LINE | cut -d'@' -f3`
+	if [ ! -f $DIST/$FILE ] ; then
+		echo " * Downloading $FILE"
+		wget -O $DIST/$FILE $URL
+	fi
+	verify_hash $HASH "$DIST/$FILE"
+	if [ $? != 0 ] ; then
+		echo " * Resume downloading $FILE"
+		wget -c -O $DIST/$FILE $URL
+
+		verify_hash $HASH "$DIST/$FILE"
+		if [ $? != 0 ] ; then
+			echo " !!! Hash mismatch for $FILE"
+			exit 1
+		fi
+	fi
+done
+echo " * Done checking distfiles"
